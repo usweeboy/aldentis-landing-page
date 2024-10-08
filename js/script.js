119961fa6b55354ce04df42a89e38c502d926bc1
usweeboy <dgrachkov03@mail.ru>
2024-10-08 15:26:12 +0500

    changes to the lazy loading video

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+const videoObserver = new IntersectionObserver((entries) => {
+  entries.forEach((video) => {
+    if (video.isIntersecting) {
+      for (let source in video.target.children) {
+        const videoSource = video.target.children[source];
 
-  if ("IntersectionObserver" in window) {
-    let lazyVideoObserver = new IntersectionObserver(function(entries) {
-      entries.forEach(function(video) {
-        if (video.isIntersecting) {
-          for (let source in video.target.children) {
-            let videoSource = video.target.children[source];
-            if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
-              videoSource.src = videoSource.dataset.src;
-            }
-          }
-
-          video.target.load();
-          video.target.classList.remove("lazy");
-          lazyVideoObserver.unobserve(video.target);
+        if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+          videoSource.src = videoSource.dataset.src;
         }
-      });
-    });
+      }
 
-    lazyVideos.forEach(function(lazyVideo) {
-      lazyVideoObserver.observe(lazyVideo);
-    });
-  }
+      video.target.load();
+      video.target.classList.remove("lazy");
+      videoObserver.unobserve(video.target);
+    }
+  });
+});
+
+document.querySelectorAll('video').forEach((video) => {
+  videoObserver.observe(video)
 });
 
 
